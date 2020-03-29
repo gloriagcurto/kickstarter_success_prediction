19a249c1469db3125c76f4273652f7ca5ea058be
Gloria G. Curto <gloria@gloriagcurto.info>
2020-03-29 18:34:07 +0200

    stemming+frequencies

--- pre_text_mining.py
+++ pre_text_mining.py
@@ -14,6 +14,9 @@ import pandas as pd
 import wordcloud
 import numpy as np
 from nltk.corpus import stopwords
+from nltk import FreqDist
+from nltk.stem import PorterStemmer
+from nltk.tokenize import sent_tokenize, word_tokenize
 
 # Generate text column
 df = pd.read_hdf('../../data/data_english.h5')
@@ -23,6 +26,7 @@ print(df.head())
 # Stop_words
 stop_words = set(stopwords.words('english'))
 stop_words.update([",", "."])
+
 
 def get_frequency_score(sentence, freqs, stop_words):
     '''
@@ -37,8 +41,13 @@ def get_frequency_score(sentence, freqs, stop_words):
                         replace(',', '').lower()
     
     sentence_words = [word for word in sentence.split() if word not in stop_words]
+    # Stemming
+    ps = PorterStemmer()
+    stemmed_words=[]
+    for w in sentence_words:
+        stemmed_words.append(ps.stem(w))
 
-    return np.mean([freqs[word] for word in sentence_words])
+    return np.mean([freqs[word] for word in stemmed_words])
 
 
 # Failed/successful:
@@ -51,19 +60,38 @@ print(cats)
 
 
 
-wc = wordcloud.WordCloud(stopwords=stop_words)
-
+wc = wordcloud.WordCloud(stopwords=stop_words, max_words=2000000)
+df_freqs = pd.DataFrame()
 for cat in cats:
+    print(f'Category: {cat}')
     for s in f_s:
-        df_sub = df[(df['category_parent_name_ori']==cat) & (df['state']==s)]
+        df_sub = df.loc[(df.category_parent_name_ori==cat) & (df.state==s)]
         text = ""
         for comment in df_sub.text : 
             text += comment
-        # lower case
-        wc.generate(text.lower())
+        text = text.replace('?', '').\
+                        replace('!', '').\
+                        replace(':', '').\
+                        replace('"', '').\
+                        replace("'", '').\
+                        replace('.', '').\
+                        replace(',', '').lower()
+        # lower case, split, and filter stop words
+        text_list = [word for word in text.split() if word not in stop_words]
+        #text_list = text_list.lower()
+        #Stemming
+        ps = PorterStemmer()
+        stemmed_words_text=[]
+        for w in text_list:
+            stemmed_words_text.append(ps.stem(w))
+        #Frequency distribution in the category
+        frequency_score = []
         for row in df_sub['text']:
-            df_sub['frequency_score'] = get_frequency_score(row, wc.words_, stop_words)
-
+           frequency_score.append(get_frequency_score(row, FreqDist(stemmed_words_text) , stop_words))
+                   
+        df_sub.loc[:,'frequency_score']= frequency_score
+    df_freqs = pd.concat([df_freqs, df_sub], axis=0)
+df_freqs.to_hdf("../../data/data_frequency_score.h5", key="frequency_score")
 '''
 Next: user_rate (based on project failure(-1)/success(+1) user history)
 '''
